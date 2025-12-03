fcb01aff6d858c6cdc9df4fd77e8c3bed5dbfa25
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-12-03 10:54:04 +0100

    add homepage and search input

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>xxx</main>
+  <main>
+    <div class="pattern" />
+    <div class="wrapper">
+      <header>
+        <img src="/hero-logo.png" alt="Hero Banner" class="mb-12" />
+        <h1>
+          Find <span class="text-gradient">Movies</span> You'll Enjoy Without
+          the Hassle
+        </h1>
+        <BaseSearch />
+      </header>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/BaseSearch.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    class="bg-primary-2/40 mx-auto mt-10 px-4 py-3 border border-white rounded-md w-full max-w-3xl"
+  >
+    <div class="relative flex items-center">
+      <img
+        src="/search.svg"
+        alt="Search icon"
+        class="left-2 absolute w-5 h-5"
+      />
+      <input
+        type="text"
+        class="bg-transparent py-2 sm:pr-10 pl-10 outline-hidden w-full text-gray-200 text-base"
+        placeholder="Search throught thousands of movies"
+      />
+    </div>
+  </div>
+</template>
